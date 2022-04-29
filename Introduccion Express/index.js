9ca4cb1053608b31d9390312d9272e8677edf259
Rafael Moctezuma <moctezumarafael26@gmail.com>
2022-04-28 22:54:51 -0500

    Introducción Express

--- Introduccion Express/index.js
+++ Introduccion Express/index.js
@@ -1,8 +1,37 @@
-const http = require("http");
+const fs = require("fs");
+const express = require("express");
+const app = express();
 const port = 5000;
 
-const server = http.createServer((req, res) => {
-  res.end("Esta es la respuesta 😎");
+// Middleware
+app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
+
+// GET
+/* app.get("/formulario", (req, res) => {
+  console.log(req.query);
+  res.send(`${req.query.nombre} El formulario fue enviado....`);
+}); */
+
+app.get("/", (req, res) => {
+  res.send("Visitaste la pagina de inicio");
 });
 
-server.listen(port, () => console.log("Funcionando 👍"));
+app.get("/registro", (req, res) => {
+  res.send("Visitaste la pagina de registro");
+});
+
+// POST
+app.post("/formulario", (req, res) => {
+  console.log(req.body);
+  const {nombre, apellido} = req.body;
+
+  if (!nombre || !apellido) return res.send("No existen los datos");
+
+  fs.writeFile(`archivos/${nombre}.txt`, apellido, (err) => {
+    if (err) return res.send("Falló al enviar el archivo");
+    res.download(__dirname + `/archivos/${nombre}.txt`);
+  });
+});
+
+app.listen(port, () => console.log("Funcionando 👍"));
