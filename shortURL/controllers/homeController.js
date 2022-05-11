459a8292805d67f1875ec7889a0a93acafc816b3
Rafael Moctezuma <moctezumarafael26@gmail.com>
2022-05-10 23:18:38 -0500

    Controlador del Home

--- shortURL/controllers/homeController.js
+++ shortURL/controllers/homeController.js
@@ -1,15 +1,43 @@
+const Url = require("../models/urls");
+const { nanoid } = require("nanoid");
+
 const leerUrl = async (req, res) => {
-	const urls = [
-		{ origin: "www.google.com/elpepe", shortURL: "afewsdwefa1" },
-		{ origin: "www.google.com/skrillex", shortURL: "afewsdwefa2" },
-		{ origin: "www.google.com/topogigo", shortURL: "afewsdwefa3" },
-	];
-	res.render("home", { urls: urls });
+	
+	try {
+		const urls = await Url.find().lean();
+		res.render("home", { urls: urls });
+	} catch (error) {
+		console.log(error);
+		res.send('falló algo');
+	}
 };
 
-const agregarUrl = async (req, res) => {};
+const agregarUrl = async (req, res) => {
+	const { origin } = req.body;
+
+	try {
+		const url = new Url({ origin: origin, shortURL: nanoid(8) });
+		await url.save();
+		res.redirect("/");
+	} catch (error) {
+		console.log(error);
+		res.send("error");
+	}
+};
+
+const eliminarUrl = async (req, res) => {
+	const {id} = req.params;
+	try {
+		await Url.findByIdAndDelete(id);
+		res.redirect("/");
+	} catch (error) {
+		console.log(error);
+		res.send("error");
+	}
+};
 
 module.exports = {
 	leerUrl,
 	agregarUrl,
+	eliminarUrl,
 };
