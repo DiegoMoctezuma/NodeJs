82d2f86cc5151d3cc515514481aae6a7d42eba45
Rafael Moctezuma <moctezumarafael26@gmail.com>
2022-05-10 23:17:33 -0500

    Router

--- shortURL/routes/home.js
+++ shortURL/routes/home.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { leerUrl, agregarUrl } = require("../controllers/homeController");
+const { leerUrl, agregarUrl, eliminarUrl } = require("../controllers/homeController");
 const router = express.Router();
 
 router.get("/", leerUrl);
 router.post("/", agregarUrl);
+router.get("/eliminar/:id", eliminarUrl);
 
 module.exports = router;
